a07b9b72b0a0560d4f1a4d04afa6ed75baf180cd
SallyNguyen2002 <anh.sally1508@gmail.com>
2025-03-19 13:58:10 -0600

    Update joke.js
    add the condition to fetch code for different joke type and category

--- joke.js
+++ joke.js
@@ -7,3 +7,25 @@ function fetchJoke(category) {
         })
     }
 
+    function displayJoke(data, category) {
+        const jokeText = document.getElementById('joke-text');
+        const title = document.getElementById('title');
+        if (data.type === 'single') {
+            jokeText.textContent = data.joke;
+        } 
+        else if (data.type === 'twopart') {
+            jokeText.innerHTML = `${data.setup} ... ${data.delivery}`;
+        }
+
+        if (category === "Any") {
+            title.textContent = "Joke Of The Day";
+        } else if (category === "Pun") {
+            title.textContent = "A Random Pun";
+        } else {
+            title.textContent = `A Random ${category} Joke`;
+        }
+    }
+    
+    function getJoke(category) {
+        fetchJoke(category);
+    };
