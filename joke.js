3ee26eb5e9ffdef6e55e78b6617de96f74322834
SallyNguyen2002 <anh.sally1508@gmail.com>
2025-03-19 14:03:50 -0600

    Update joke.js
    reorganize code

--- joke.js
+++ joke.js
@@ -7,29 +7,28 @@ function fetchJoke(category) {
         })
     }
 
-    function displayJoke(data, category) {
-        const jokeText = document.getElementById('joke-text');
-        const title = document.getElementById('title');
-        if (data.type === 'single') {
-            jokeText.textContent = data.joke;
-        } 
-        else if (data.type === 'twopart') {
-            jokeText.innerHTML = `${data.setup} ... ${data.delivery}`;
-        }
-
-        if (category === "Any") {
-            title.textContent = "Joke Of The Day";
-        } else if (category === "Pun") {
-            title.textContent = "A Random Pun";
-        } else {
-            title.textContent = `A Random ${category} Joke`;
-        }
+function displayJoke(data, category) {
+    const jokeText = document.getElementById('joke-text');
+    const title = document.getElementById('title');
+    if (category === "Any") {
+        title.textContent = "Joke Of The Day";
+    } else if (category === "Pun") {
+        title.textContent = "A Random Pun";
+    } else {
+        title.textContent = `A Random ${category} Joke`;
     }
-    
-    window.onload = function() {
-        fetchJoke('Any');
+    if (data.type === 'single') {
+        jokeText.textContent = data.joke;
+    } 
+    else if (data.type === 'twopart') {
+        jokeText.innerHTML = `${data.setup} ... ${data.delivery}`;
     }
+}
 
-    function getJoke(category) {
-        fetchJoke(category);
-    };
+window.onload = function() {
+    fetchJoke('Any');
+}
+
+function getJoke(category) {
+    fetchJoke(category);
+};
